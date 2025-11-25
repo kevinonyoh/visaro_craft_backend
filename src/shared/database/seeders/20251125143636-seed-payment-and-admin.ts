995338549a13f9fb04d1221ed4cde6e6372eafad
kevinonyoh <evionyoh@gmail.com>
2025-11-25 16:05:13 +0100

    payment and admin seed

--- src/shared/database/seeders/20251125143636-seed-payment-and-admin.ts
+++ src/shared/database/seeders/20251125143636-seed-payment-and-admin.ts
@@ -6,32 +6,32 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = {
   async up(queryInterface, Sequelize) {
     // Seed payment_options table
-    await queryInterface.bulkInsert('payment_options', [
-      // {
-      //   id: uuidv4(),
-      //   name: 'CONSULTATION',
-      //   amount: 250,
-      //   currency: 'usd',
-      //   created_at: new Date(),
-      //   updated_at: new Date(),
-      // },
-      // {
-      //   id: uuidv4(),
-      //   name: 'PETITION_PREPARATION',
-      //   amount: 4000,
-      //   currency: 'usd',
-      //   created_at: new Date(),
-      //   updated_at: new Date(),
-      // },
-      // {
-      //   id: uuidv4(),
-      //   name: 'REVIEW_PETITION',
-      //   amount: 4000,
-      //   currency: 'usd',
-      //   created_at: new Date(),
-      //   updated_at: new Date(),
-      // },
-    ], {});
+    // await queryInterface.bulkInsert('payment_options', [
+    //   // {
+    //   //   id: uuidv4(),
+    //   //   name: 'CONSULTATION',
+    //   //   amount: 250,
+    //   //   currency: 'usd',
+    //   //   created_at: new Date(),
+    //   //   updated_at: new Date(),
+    //   // },
+    //   // {
+    //   //   id: uuidv4(),
+    //   //   name: 'PETITION_PREPARATION',
+    //   //   amount: 4000,
+    //   //   currency: 'usd',
+    //   //   created_at: new Date(),
+    //   //   updated_at: new Date(),
+    //   // },
+    //   // {
+    //   //   id: uuidv4(),
+    //   //   name: 'REVIEW_PETITION',
+    //   //   amount: 4000,
+    //   //   currency: 'usd',
+    //   //   created_at: new Date(),
+    //   //   updated_at: new Date(),
+    //   // },
+    // ], {});
 
     // Seed admins table
     const password = process.env.ADMIN_PASSWORD || 'Admin@123';
